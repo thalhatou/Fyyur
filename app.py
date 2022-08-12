f9a91096c34b2316821ada42250e3f18e38deaa4
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2022-08-12 20:12:24 +0100

    done implementing model c;asses and their relationships

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ migrate = Migrate(app,db)
 # Models.
 #----------------------------------------------------------------------------#
 
+
+
+
+# The Venue class is a model that represents a venue in the database.
 class Venue(db.Model):
     __tablename__ = 'venues'
 
@@ -43,14 +47,19 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean,default=False)
     seeking_description = db.Column(db.String(600))
+    Shows = db.relationship('show')
+    venues = db.relationship('Venue',secondary='shows',back_populates='venues')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
+# The Artist class is a model for the artists table in the database. It has the following columns: id,
+# name, city, state, phone, genres, image_link, facebook_link, website_link, seeking_venue, and
+# seeking_description.
 class Artist(db.Model):
     __tablename__ = 'artists'
 
@@ -59,12 +68,14 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean,default=False)
     seeking_description = db.Column(db.String(600))
+    Shows = db.relationship('show')
+    venues = db.relationship('Venue',secondary='shows',back_populates='artists')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -74,11 +85,12 @@ class Show(db.Model):
   __tablename__ = 'shows'
   id = db.Column(db.integer,primary_key=True)
   show_start_time = db.Column(db.DateTime, nullable=False)
+  
+# Creating a relationship between the two tables(Artist & Venue).
   artist = db.relationship('Artist')
-  artist_id = db.Column(db.Integer,db.ForeignKey('artists.id',ondelete='CASCADE'))
-
-  show = db.relationship('Show')
-  show_id = db.Column(db.Integer,db.ForeignKey('Shows.id',ondelete='CASCADE'))
+  artist_id = db.Column(db.Integer,db.ForeignKey('artists.id',ondelete='CASCADE',nullable=False))
+  venue = db.relationship('Venue')
+  venue_id = db.Column(db.Integer,db.ForeignKey('venues.id',ondelete='CASCADE',nullable=False))
 
   
 #----------------------------------------------------------------------------#
