3618b982b4b76aa95c19fcc48850f73d0af21c35
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2022-08-16 01:30:56 +0100

    heroku defualt cofig remove

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-config = {
-'default' : ProductionConfig
-}
+# config = {
+# 'default' : ProductionConfig
+# }
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:7y8a1h64@localhost:5432/fyyurdb'
 
